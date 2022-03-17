458d7bc66e6aebfa7b955f2ee3239c9b5eb3f505
AnaLinsDev <analinsdev@gmail.com>
2022-03-17 10:37:18 -0300

    changes

---
+++ dist/_nuxt/1250cd4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{130:function(n,o,w){w(131),n.exports=w(132)}},[[130,3,1,4]]]);
--- nuxt.config.js
+++ nuxt.config.js
@@ -2,24 +2,20 @@ export default {
 
   target: 'static',
 
+  router: {
+    base: '/quest-app/'
+  },
+
   ssr: false,
 
   components: true,
-
-  css: [
-    '@/assets/css/global.css'
-  ],
   
   buildModules: [
     '@nuxtjs/vuetify',
   ],
 
-  router: {
-    middleware: ['auth', 'adminAuth']
-  },
-
   head: {
-    titleTemplate: 'RM - Nuxt',
+    titleTemplate: 'Quiz - Nuxt',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
