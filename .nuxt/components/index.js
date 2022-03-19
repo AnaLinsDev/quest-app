6b137b111a63ab62dd31f6a734d3474bbb53da20
AnaLinsDev <analinsdev@gmail.com>
2022-03-19 14:04:40 -0300

    add pt2

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,7 +1,7 @@
-export { default as CommonMenu } from '../..\\components\\common\\menu.vue'
-export { default as CommonSideBar } from '../..\\components\\common\\side-bar.vue'
-export { default as UserLoginForm } from '../..\\components\\user\\login-form.vue'
-export { default as UserRegisterForm } from '../..\\components\\user\\register-form.vue'
+export const CommonMenu = () => import('../..\\components\\common\\menu.vue' /* webpackChunkName: "components/common-menu" */).then(c => wrapFunctional(c.default || c))
+export const CommonSideBar = () => import('../..\\components\\common\\side-bar.vue' /* webpackChunkName: "components/common-side-bar" */).then(c => wrapFunctional(c.default || c))
+export const UserLoginForm = () => import('../..\\components\\user\\login-form.vue' /* webpackChunkName: "components/user-login-form" */).then(c => wrapFunctional(c.default || c))
+export const UserRegisterForm = () => import('../..\\components\\user\\register-form.vue' /* webpackChunkName: "components/user-register-form" */).then(c => wrapFunctional(c.default || c))
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
